dc0ca51b5d1c1c209d35cc01d5f3dbad9d73d2c7
sebastian <sebaacc11@gmail.com>
2025-04-10 11:43:53 -0300

    clase 10 de abril

---
+++ PryEdMarkoja/clsListaSimple.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PryEdMarkoja
+{
+    internal class clsListaSimple
+    {
+        private clsNodo pri;
+        public clsNodo Primero
+        {
+            get { return pri; }
+            set { pri = value; }
+        }
+
+        public void agregar(clsNodo Nuevo)
+        {
+            if (Primero == null)
+            {
+                Primero = Nuevo;
+            }
+            else
+            {
+                if (Nuevo.Codigo <=  Primero.Codigo)
+                {
+                    Nuevo.Siguiente = Primero;
+                    Primero = Nuevo;
+                }
+                else
+                {
+                    clsNodo aux = Primero;
+                    clsNodo ant = Primero;
+                    while(aux != null && Nuevo.Codigo > aux.Codigo)
+                    {
+                        ant = aux;
+                        aux = aux.Siguiente;
+                    }
+                    ant.Siguiente = Nuevo;
+                    Nuevo.Siguiente = aux;
+                }
+            }
+        }
+        public void eliminar(Int32 Codigo)
+        {
+            if (Primero.Codigo == Codigo)
+            {
+                Primero = Primero.Siguiente;
+            } 
+            else
+            {
+                clsNodo ant = Primero;
+                clsNodo aux= Primero;
+                while(aux.Codigo != Codigo)
+                {
+                    ant = aux;
+                    aux = aux.Siguiente;
+                }
+                ant.Siguiente = aux.Siguiente;
+            }
+        }
+
+        public void Recorrer(DataGridView Grilla)
+        {
+            clsNodo aux = Primero;
+            Grilla.Rows.Clear();
+            while (aux != null)
+            {
+                Grilla.Rows.Add(aux.Codigo, aux.Nombre, aux.Tramite);
+                aux = aux.Siguiente;
+            }
+        }
+        public void Recorrer(ListBox Lista)
+        {
+            clsNodo aux = Primero;
+            Lista.Items.Clear();
+            while (aux != null)
+            {
+                Lista.Items.Add(aux.Codigo + " " + aux.Nombre + " " + aux.Tramite);
+                aux = aux.Siguiente;
+            }
+        }
+
+        public void Recorrer(ComboBox ComboBox)
+        {
+            clsNodo aux = Primero;
+            ComboBox.Items.Clear();
+            while (aux != null)
+            {
+                ComboBox.Items.Add(aux.Nombre);
+                aux = aux.Siguiente;
+            }
+        }
+        public void Recorrer() //para CSV
+        {
+            clsNodo aux = Primero;
+            StreamWriter AD = new StreamWriter("Lista.csv", false, Encoding.UTF8);
+            AD.WriteLine("Lista de espera\n");
+            AD.WriteLine("Codigo;Nombre;Tramite");
+            while (aux != null)
+            {
+                AD.Write(aux.Codigo);
+                AD.Write(";");
+                AD.Write(aux.Nombre);
+                AD.Write(";");
+                AD.WriteLine(aux.Tramite);
+                aux = aux.Siguiente;
+            }
+            AD.Close();
+        }
+        public void Recorrer(String NombreArchivo)
+        {
+            clsNodo aux = Primero;
+            StreamWriter AD = new StreamWriter(NombreArchivo, false, Encoding.UTF8);
+            AD.WriteLine("Lista de espera\n");
+            AD.WriteLine("Código;Nombre;Trámite");
+            while (aux != null)
+            {
+                AD.Write(aux.Codigo);
+                AD.Write(";");
+                AD.Write(aux.Nombre);
+                AD.Write(";");
+                AD.WriteLine(aux.Tramite);
+                aux = aux.Siguiente;
+            }
+            AD.Close();
+        }
+
+    }
+}
